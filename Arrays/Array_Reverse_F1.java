7c5125e4745d3dde15262c3b0ced9b14de6d5f2a
banzom <aditya.panwar.2208@gmail.com>
2023-08-16 13:12:16 +0530

    Reverse of Array

---
+++ Arrays/Array_Reverse_F1.java
@@ -0,0 +1,33 @@
+public class Array_Reverse_F1 {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, -8, 7, 3, -2 };
+        System.out.print("Original Array: ");
+        printArray(arr);
+        reverseArray(arr);
+        System.out.print("Reversed Array: ");
+        printArray(arr);
+    }
+
+    static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    static void reverseArray(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
