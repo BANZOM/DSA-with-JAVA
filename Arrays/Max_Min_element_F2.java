1833f1e86ba8a4b9056659735e20336b8f16b58e
banzom <aditya.panwar.2208@gmail.com>
2023-08-16 14:52:13 +0530

    Instance (better encapsulation)

--- Arrays/Max_Min_element_F2.java
+++ Arrays/Max_Min_element_F2.java
@@ -1,5 +1,15 @@
 public class Max_Min_element_F2 {
     public static void main(String[] args) {
-        
+        int[] arr = { 1, 2, 3, -8, 7, 3, -2 };
+        System.out.print("Array: ");
+        Max_Min_element_F2 instance = new Max_Min_element_F2();
+        instance.printArray(arr);
     }
+
+    void printArray(int[] arr) {
+        for (int i: arr)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+    
 }
