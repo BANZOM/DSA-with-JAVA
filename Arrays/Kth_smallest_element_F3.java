4240d2f2bdd192f88e652c3f89aa241b4483e65c
banzom <aditya.panwar.2208@gmail.com>
2023-08-17 02:20:42 +0530

    new file:   Arrays/Kth_smallest_element_F3.java

---
+++ Arrays/Kth_smallest_element_F3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Kth_smallest_element_F3 {
+    public static void main(String[] args) {
+        // Input array
+        System.out.print("Enter the size of array: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        
+    }
+}
