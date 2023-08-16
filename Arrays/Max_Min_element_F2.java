e5d86803d90f9fcf773de440fb3bd0e0038b4ecd
banzom <aditya.panwar.2208@gmail.com>
2023-08-16 20:12:58 +0530

    modified:   Arrays/Max_Min_element_F2.java

--- Arrays/Max_Min_element_F2.java
+++ Arrays/Max_Min_element_F2.java
@@ -1,15 +1,49 @@
 public class Max_Min_element_F2 {
+
+    /**
+     * Nested class 'Max_Min' to store the minimum and maximum values.
+     */
+    static class Max_Min {
+        int min, max;
+
+        Max_Min(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, -8, 7, 3, -2 };
+        int[] arr = {};
         System.out.print("Array: ");
+
         Max_Min_element_F2 instance = new Max_Min_element_F2();
         instance.printArray(arr);
+
+        Max_Min answer = instance.getMaxMin(arr);
+        System.out.println("Max: " + answer.max);
+        System.out.println("Min: " + answer.min);
+
     }
 
     void printArray(int[] arr) {
-        for (int i: arr)
+        for (int i : arr)
             System.out.print(i + " ");
         System.out.println();
     }
-    
+
+    Max_Min getMaxMin(int[] arr) {
+        if (arr.length == 0) {
+            return new Max_Min(Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        Max_Min answer = new Max_Min(Integer.MAX_VALUE, Integer.MIN_VALUE);
+
+        for (int i : arr) {
+            answer.min = Math.min(i, answer.min);
+            answer.max = Math.max(i, answer.max);
+        }
+
+        return answer;
+    }
+
 }
