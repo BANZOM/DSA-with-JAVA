037bb343598480ce196a625837740ce1324f30a4
banzom <aditya.panwar.2208@gmail.com>
2023-08-17 08:53:45 +0530

    modified:   Arrays/F3_Kth_max_min_element.java

--- Arrays/F3_Kth_max_min_element.java
+++ Arrays/F3_Kth_max_min_element.java
@@ -1,5 +1,9 @@
+import java.sql.Array;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.util.Collections;
+
 public class F3_Kth_max_min_element {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -21,5 +25,25 @@ public class F3_Kth_max_min_element {
 
         System.out.println("Kth smallest element is: " + kthSmallest);
         System.out.println("Kth maximum element is: " + kthLargest);
+
+        int[] answer = kthByQueue(arr, k);
+        System.out.println("Kth smallest element (PQ) is: " + answer[0]);
+        System.out.println("Kth maximum element (PQ) is: " + answer[1]);
+    }
+
+    static int[] kthByQueue(int[] arr, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();   // by default min heap
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());   // for max heap
+        for (int i : arr) {
+            minHeap.add(i);
+            maxHeap.add(i);
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            minHeap.poll();
+            maxHeap.poll();
+        }
+
+        return new int[]{minHeap.peek(), maxHeap.peek()};
     }
 } 
