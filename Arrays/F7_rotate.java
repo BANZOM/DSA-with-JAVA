47fb813a0e80726d2a726287f7e8d05c757e89c7
banzom <aditya.panwar.2208@gmail.com>
2023-08-23 09:50:35 +0530

    modified:   Arrays/F7_rotate.java

--- Arrays/F7_rotate.java
+++ Arrays/F7_rotate.java
@@ -1,6 +1,33 @@
 import java.util.Scanner;
 
 class F7_rotate {
+    /**
+     * Rotates the given array by one position in a clockwise direction.
+     *
+     * @param arr The array to be rotated.
+     */
+    static void rotate(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+        
+        int lastElement = arr[arr.length - 1];
+        System.arraycopy(arr, 0, arr, 1, arr.length - 1);
+        arr[0] = lastElement;
+    }
+
+    /**
+     * Prints the elements of the given array.
+     *
+     * @param arr The array to be printed.
+     */
+    static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         // Given an array, rotate the array by one position in clock-wise direction.
         /*
@@ -22,22 +49,5 @@ class F7_rotate {
         rotate(arr);
         System.out.print("Rotated Array: ");
         printArray(arr);
-    }
-
-    static void rotate(int[] arr) {
-        if (arr.length != 1) {
-            int temp = arr[arr.length - 1];
-            for (int i = arr.length - 1; i > 0; i--) {
-                arr[i] = arr[i - 1];
-            }
-            arr[0] = temp;
-        }
-    }
-
-    static void printArray(int[] arr) {
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
     }
 }
