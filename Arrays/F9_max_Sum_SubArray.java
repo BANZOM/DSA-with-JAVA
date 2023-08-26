60f2bff52f240a498c9ade376d2e2d8397ae1ed3
banzom <aditya.panwar.2208@gmail.com>
2023-08-26 22:54:38 +0530

    modified:   Arrays/F9_max_Sum_SubArray.java

--- Arrays/F9_max_Sum_SubArray.java
+++ Arrays/F9_max_Sum_SubArray.java
@@ -1,11 +1,18 @@
 class F9_max_Sum_SubArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,-10,11};
+        int[] arr = { 1, 2, 3, 4, -10, 11 };
 
-        System.out.println("The Max Sum of Subarray is: " + getMaxSum(arr,arr.length));
-        
+        System.out.println("The Max Sum of Subarray is: " + getMaxSum(arr, arr.length));
+
     }
 
+    /**
+     * Finds the maximum sum of a subarray in an array of integers.
+     *
+     * @param arr The array of integers.
+     * @param n   The length of the array.
+     * @return The maximum sum of a subarray.
+     */
     static long getMaxSum(int[] arr, int n) {
         long max = Integer.MIN_VALUE;
         long sum = 0;
@@ -14,7 +21,8 @@ class F9_max_Sum_SubArray {
             sum += arr[i];
             max = Math.max(max, sum);
 
-            if(sum < 0) sum = 0;
+            if (sum < 0)
+                sum = 0;
         }
 
         return max;
