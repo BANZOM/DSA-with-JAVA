ebdb36e7ee020505872e4f9d4b694616d5de1ed5
Aditya Panwar <adityap@cabin4j.com>
2024-02-19 14:22:26 +0530

    find-repeating-element-sorted-array-size-n/

---
+++ Programming_Pathshala/Q1.java
@@ -0,0 +1,29 @@
+// getting dublicate in sorted array
+/**
+ * Q1: https://www.geeksforgeeks.org/find-repeating-element-sorted-array-size-n/
+ */
+public class Q1 {
+    static int answer(int[] arr) {
+        int ans = -1;
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            int c = mid+1;
+
+            if (c > arr[mid]) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 3, 4};
+        System.out.println(answer(arr));
+    }
+    
+}
