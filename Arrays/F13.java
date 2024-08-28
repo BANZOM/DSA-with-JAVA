5d2e7a4449208d22806cef3aecb3deb30f4b320f
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-08-28 15:57:49 +0530

    Solution of F13

--- Arrays/F13.java
+++ Arrays/F13.java
@@ -7,9 +7,9 @@
 public class F13 {
 
     public static void main(String[] args) {
-        int[] arr = new int[] {1, 2, 3, 4, 5};
+        int[] arr = new int[] {-11, 2, 3, 4, 5};
         int N = 6;
-        int P = 1;
+        int P = -1;
         int Q = 2;
         int R = 3;
 
@@ -20,41 +20,41 @@ public class F13 {
     private static long calculateMaxValue(int[] arr, int n, int p, int q, int r) {
         long ans = Long.MIN_VALUE;
 
-        // int[] prefixMax = prefixMaxArray(arr);
-        int[] suffixMax = suffixMaxArray(arr);
-
+        // int[] prefixMax = prefixMaxArray(arr); // only suffixMaxArray is sufficient
+        int[] suffixMax = suffixMaxArray(arr,r);
         int[] maxThree = new int[3];
-        int prefixMax = arr[0];
+        int prefixMax = arr[0]*p;
+
         for(int i=1; i<arr.length-1; i++) {
-            prefixMax = Math.max(prefixMax,arr[i-1]);
-            if(prefixMax + arr[i] + suffixMax[i+1] > ans) {
+            if(prefixMax + arr[i]*q + suffixMax[i+1] > ans) {
                 maxThree[0] = prefixMax;
-                maxThree[1] = arr[i];
+                maxThree[1] = arr[i]*q;
                 maxThree[2] = suffixMax[i+1];
-                ans = prefixMax + arr[i] + suffixMax[i+1];
+                ans = maxThree[0] + maxThree[1] + maxThree[2];
             }
+            prefixMax = Math.max(prefixMax,arr[i]*p);
         }
 
         System.out.println(maxThree[0] + " " + maxThree[1] + " " + maxThree[2]);
 
-        return p*maxThree[0] + q* maxThree[1] + r*maxThree[2];
+        return ans;
     }
 
-    private static int[] prefixMaxArray(int[] arr) {
+    private static int[] prefixMaxArray(int[] arr, int x) {
         int[] prefixMax = new int[arr.length];
-        prefixMax[0] = arr[0];
+        prefixMax[0] = arr[0]*x;
         for (int i = 1; i < prefixMax.length; i++) {
-            prefixMax[i] = Math.max(prefixMax[i-1], arr[i]);
+            prefixMax[i] = Math.max(prefixMax[i-1], arr[i]*x);
         }
         return prefixMax;
     } 
     
-    private static int[] suffixMaxArray(int[] arr) {
+    private static int[] suffixMaxArray(int[] arr, int x) {
         int[] suffixMax = new int[arr.length];
-        suffixMax[arr.length - 1] = arr[arr.length-1];
+        suffixMax[arr.length - 1] = arr[arr.length-1]*x;
 
         for (int i = arr.length-2; i >= 0; i--) {
-            suffixMax[i] = Math.max(suffixMax[i+1], arr[i]);
+            suffixMax[i] = Math.max(suffixMax[i+1], arr[i]*x);
         }
 
         return suffixMax;
