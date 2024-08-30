e6e8064cd6ac6603d45ec1106f7e1c9fa2b18556
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-08-30 22:43:09 +0530

    Question F16

---
+++ Arrays/F16_modify_the_array_1.java
@@ -0,0 +1,17 @@
+/*
+ * Given an array of size N, contains 0 to N-1 elements, all distinct.
+ * Modify the array such that: if A[i] = j, then transform it to A[j] = i;
+ */
+public class F16_modify_the_array_1 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0,3,4,2,1};
+        
+        printModifiedArray(arr, arr.length);
+    }
+
+    static void printModifiedArray(int[] arr, int size) {
+        
+    }
+    
+}
