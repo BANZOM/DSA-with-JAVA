fb5ceacd4befcfb744e81848df7e51ec0931bc93
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-08-31 10:39:32 +0530

    F18: Brute Force Solution

--- Arrays/F18_LCM_GDC.java
+++ Arrays/F18_LCM_GDC.java
@@ -3,8 +3,8 @@
  */
 public class F18_LCM_GDC {
     public static void main(String[] args) {
-        Long num1 = 9L;
-        Long num2 = 15L;
+        Long num1 = 144L;
+        Long num2 = 233L;
 
         Long[] ans = getLcmAndGcd(num1, num2);
 
@@ -13,6 +13,25 @@ public class F18_LCM_GDC {
     }
 
     static Long[] getLcmAndGcd(Long num1, Long num2) {
+        Long[] ans = new Long[2];
+        
+        ans[1] = getGCD(num1, num2); // GCd
 
+        Long max = Math.max(num1, num2);
+        Long min = Math.min(num1, num2);
+        Long tmp = max;
+        int i = 2;
+        while (max % min != 0) {
+            max = tmp * i;
+            i++;
+        }
+        ans[0] = max; // LCM
+
+        return ans;
+    }
+
+    static Long getGCD(Long num1, Long num2) {
+        if(num1 % num2 == 0) return num2;
+        return getGCD(num2, num1%num2);
     }
 }
