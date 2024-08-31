b0f02b646b4bc9c888bb6e9e03b72ab61dbea221
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-08-31 10:22:15 +0530

    F18: Question

---
+++ Arrays/F18_LCM_GDC.java
@@ -0,0 +1,18 @@
+/*
+ * Given two numbers, Find the GCD and LCM of them
+ */
+public class F18_LCM_GDC {
+    public static void main(String[] args) {
+        Long num1 = 9L;
+        Long num2 = 15L;
+
+        Long[] ans = getLcmAndGcd(num1, num2);
+
+        System.out.println("LCM: " + ans[0]);
+        System.out.println("GCD: " + ans[1]);
+    }
+
+    static Long[] getLcmAndGcd(Long num1, Long num2) {
+
+    }
+}
