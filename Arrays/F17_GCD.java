d4f1f29466740d8a2cfb50295cccd3b56c8b5edd
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-08-31 10:17:27 +0530

    F17: Solution with Recursion

--- Arrays/F17_GCD.java
+++ Arrays/F17_GCD.java
@@ -3,21 +3,15 @@
  */
 public class F17_GCD {
     public static void main(String[] args) {
-        int num1 = 9999991;
-        int num2 = 9999992;
+        int num1 = 7;
+        int num2 = 25;
 
         System.out.println(findGCD(num1, num2));
         
     }
 
     static int findGCD(int num1, int num2) {
-
-        while (num1 % num2 != 0) {
-            int tmp = num2;
-            num2 = num1 % num2;
-            num1 = tmp;
-        }
-
-        return num2;
+        if(num1 % num2 == 0) return num2;
+        return findGCD(num2, num1%num2);
     }
 }
