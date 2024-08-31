6609b3c0a9c6eff4bc4871c8614047d6d6cb2bf3
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-08-31 10:49:04 +0530

    F18: Better Solution

--- Arrays/F18_LCM_GDC.java
+++ Arrays/F18_LCM_GDC.java
@@ -3,29 +3,25 @@
  */
 public class F18_LCM_GDC {
     public static void main(String[] args) {
-        Long num1 = 144L;
-        Long num2 = 233L;
+        Long num1 = 9999991L;
+        Long num2 = 9999992L;
 
         Long[] ans = getLcmAndGcd(num1, num2);
 
         System.out.println("LCM: " + ans[0]);
         System.out.println("GCD: " + ans[1]);
     }
+ 
 
+    /*
+     * LCM(a,b) * HCF(a,b) = a * b
+     */
     static Long[] getLcmAndGcd(Long num1, Long num2) {
         Long[] ans = new Long[2];
         
         ans[1] = getGCD(num1, num2); // GCd
 
-        Long max = Math.max(num1, num2);
-        Long min = Math.min(num1, num2);
-        Long tmp = max;
-        int i = 2;
-        while (max % min != 0) {
-            max = tmp * i;
-            i++;
-        }
-        ans[0] = max; // LCM
+        ans[0] = (num1*num2) / ans[1]; // LCM
 
         return ans;
     }
