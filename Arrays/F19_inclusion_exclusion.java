ec2cd258e181e840611159714d0d4a4187baeb1a
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-09-02 10:33:31 +0530

    F19: Question - based on Inclusion Exclusion

---
+++ Arrays/F19_inclusion_exclusion.java
@@ -0,0 +1,21 @@
+/**
+ * F19_inclusion_exclusion
+ * Given a number N, and two nums A and B
+ * Find the count of numbers from 1 to N which are divisible by either A or B
+ */
+public class F19_inclusion_exclusion {
+
+    public static void main(String[] args) {
+        int N = 20;
+        int A = 4;
+        int B = 6;
+        
+        System.out.println(count(N, A, B));
+    }
+
+    private static int count(int N, int A, int B) {
+        int ans = 0;
+
+        return ans;
+    }
+}
