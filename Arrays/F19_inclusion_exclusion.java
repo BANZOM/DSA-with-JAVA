dc186d0affd384d678c6bbe2bc25492154adfe4f
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-09-02 10:39:36 +0530

    F19: Basic solution

--- Arrays/F19_inclusion_exclusion.java
+++ Arrays/F19_inclusion_exclusion.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * F19_inclusion_exclusion
  * Given a number N, and two nums A and B
@@ -9,13 +10,19 @@ public class F19_inclusion_exclusion {
         int N = 20;
         int A = 4;
         int B = 6;
-        
+
         System.out.println(count(N, A, B));
     }
 
-    private static int count(int N, int A, int B) {
+    static int count(int N, int A, int B) {
         int ans = 0;
-
+        
+        for (int i = 1; i <= N; i++) {
+            if (i%A == 0 || i%B == 0) {
+                ans++;
+            }
+        }
+        
         return ans;
     }
 }
