bbcbfe446d6e639f2d13f0326d760a35a4be2abe
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-09-05 10:44:02 +0530

    F20: Concept of Factorisation

---
+++ Arrays/F20_Factorisation_Concept.java
@@ -0,0 +1,17 @@
+/*
+ * Find the factors of Number N
+ */
+public class F20_Factorisation_Concept {
+    public static void main(String[] args) {
+        int N = 20;
+        printFactors(N);
+    }
+
+    static void printFactors(int N) {
+        for(int i=1; i<=N; i++) {
+            if (N%i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
