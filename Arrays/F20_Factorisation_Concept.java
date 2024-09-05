7733e29e5e9b05cef3357521aa7971a177190756
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-09-05 13:06:10 +0530

    F20: Added time calculation

--- Arrays/F20_Factorisation_Concept.java
+++ Arrays/F20_Factorisation_Concept.java
@@ -1,11 +1,12 @@
 import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 /*
  * Find the factors of Number N
  */
 public class F20_Factorisation_Concept {
     public static void main(String[] args) {
-        int N = 20;
+        int N = 9_999_999;
         printFactors(N);
     }
 
@@ -13,8 +14,9 @@ public class F20_Factorisation_Concept {
         int sqrtOfN = (int) Math.sqrt(N);
         // System.out.println(sqrtOfN);
 
-        List<Integer> list = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
 
+        long start = System.nanoTime();
         for(int i=1; i<=sqrtOfN; i++) {
             if (N%i == 0) {
                 System.out.println(i);
@@ -22,5 +24,7 @@ public class F20_Factorisation_Concept {
             }
         }
         list.forEach(System.out::println);
+
+        System.out.println("Time taken: " + (System.nanoTime() - start));
     }
 }
