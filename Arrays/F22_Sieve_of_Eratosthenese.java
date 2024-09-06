a3c8f45fde4bc8eb1097ae701d32b1d2ab41958c
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-09-06 08:57:43 +0530

    F22: Solution

--- Arrays/F22_Sieve_of_Eratosthenese.java
+++ Arrays/F22_Sieve_of_Eratosthenese.java
@@ -4,14 +4,30 @@ import java.util.*;
  */
 public class F22_Sieve_of_Eratosthenese {
     public static void main(String[] args) {
-        int N = 100;
+        int N = 10000;
         List<Integer> primeNumbers = seiveOfEratosthenese(N);
-        primeNumbers.forEach(e -> System.out.println(e + " "));
+        primeNumbers.forEach(e -> System.out.print(e + " "));
+        System.out.println();
     }
 
     static List<Integer> seiveOfEratosthenese(int N) {
-        List<Integer> primeNumbers = new ArrayList<>();
+        int[] prime = new int[N];
         
-        return primeNumbers;
+        for(int i=1; i<=N; i++) prime[i-1] = i;
+        prime[0] = 0;
+        for(int i=0; i<N; i++) {
+            int val = prime[i];
+            if(val != 0)
+                for(int j = val*val; j<=N; j+=val) {
+                    prime[j-1] = 0;
+                }
+        }
+        
+        List<Integer> primeList = new ArrayList<>();
+        
+        for(int i=0; i<N; i++)
+            if(prime[i] != 0) primeList.add(prime[i]);
+            
+        return primeList;
     }
 }
