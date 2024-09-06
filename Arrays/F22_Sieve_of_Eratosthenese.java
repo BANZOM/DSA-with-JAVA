92468c4f79bb93aaf152e80682aa231623b4db19
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-09-06 08:25:14 +0530

    F22: Problem : finding prime numbers

---
+++ Arrays/F22_Sieve_of_Eratosthenese.java
@@ -0,0 +1,17 @@
+import java.util.*;
+/*
+ * Pringing the prime num upto 1 to N
+ */
+public class F22_Sieve_of_Eratosthenese {
+    public static void main(String[] args) {
+        int N = 100;
+        List<Integer> primeNumbers = seiveOfEratosthenese(N);
+        primeNumbers.forEach(e -> System.out.println(e + " "));
+    }
+
+    static List<Integer> seiveOfEratosthenese(int N) {
+        List<Integer> primeNumbers = new ArrayList<>();
+        
+        return primeNumbers;
+    }
+}
