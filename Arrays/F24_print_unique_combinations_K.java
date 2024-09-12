4fc5a3fdcc82ac583c6ff13eb81b9273001119c5
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-09-12 08:51:34 +0530

    F24: Question , base template

---
+++ Arrays/F24_print_unique_combinations_K.java
@@ -0,0 +1,17 @@
+import java.util.*;
+/*
+ * Given N and K, print all unique combinations of K elements from 1 to N
+ * eg: N = 4, K = 2
+ * then unique combinations are: [1,2], [1,3], [1,4], [2,3], [2,4], [3,4]
+ */
+public class F24_print_unique_combinations_K {
+    public static void main(String[] args) {
+        int N = 4;
+        int K = 2;
+        List<Integer> output = method();
+        System.out.println(output);
+    }
+
+    
+    
+}
