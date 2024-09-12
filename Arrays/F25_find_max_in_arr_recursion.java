9c42ef5927cb2e4bdf3b786a24af5cab1bfc1364
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-09-12 12:22:07 +0530

    F25: Divide and Conquer solution

--- Arrays/F25_find_max_in_arr_recursion.java
+++ Arrays/F25_find_max_in_arr_recursion.java
@@ -3,21 +3,21 @@
  */
 public class F25_find_max_in_arr_recursion {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,10,3,2,1,5,50};
+        int[] arr = new int[]{1,2,3,10,3,2,1,5};
         System.out.println(findMax(arr));
+
     }
 
     static int findMax(int[] arr) {
-        if(arr.length == 0) return -1;
+        if(arr.length == 0) return -1; // Not exist
         if(arr.length == 1) return arr[0];
 
-        return method(arr,arr.length,1, arr[0]);
+        return method(arr,0,arr.length-1); // Divide and Conquer
     }
 
-    static int method(int[] arr, int N, int idx, int prevMax) {
-        if(idx != N) {
-            prevMax = prevMax < arr[idx] ? method(arr,N,idx+1,arr[idx]) : method(arr, N, idx+1, prevMax);
-        }
-        return prevMax;
+    private static int method(int[] arr, int i, int j) {
+        if(i == j) return arr[i];
+        int mid = i+(j-i)/2;
+        return Math.max(method(arr, i, mid), method(arr, mid+1, j));
     }
 }
