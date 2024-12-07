2eb544a7b601d4ec8177dc91d349ba76c566681c
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-12-07 12:32:35 +0530

    Approach 2: Refactor F001FreqOfFreqElement to optimize frequency calculation and improve performance

--- slidingwindow/F001FreqOfFreqElement.java
+++ slidingwindow/F001FreqOfFreqElement.java
@@ -4,27 +4,53 @@ import java.util.Arrays;
 
 public class F001FreqOfFreqElement {
     public static void main(String[] args) {
-        int[] nums = { 4, 3, 13, 1, 8 };
+        int[] nums = { 1,2,4};
         int k = 5;
         System.err.println(solve(nums, k));
     }
 
     private static int solve(int[] nums, int k) {
-        int max = 1;
+        int freq = 1;
         Arrays.sort(nums);
 
-        for (int i = nums.length - 1; i > 0; i--) {
-            int t = k;
-            int j = i - 1;
-            while (j >= 0 && t > 0) {
-                t -= nums[i] - nums[j];
-                if (t >= 0) {
-                    max = Math.max(max, i - j + 1);
-                    j--;
-                }
-            }
+        int[] prefixSumArray = prefixSumArray(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            int leftCompatible = getLeftCompatible(nums, k, 0, i, prefixSumArray);
+            freq = Math.max(freq, i - leftCompatible + 1);
         }
 
-        return max;
+        return freq;
+    }
+
+    private static int getLeftCompatible(int[] nums, int k, int left, int right, int[] prefixSumArray) {
+        int i = left;
+        int j = right;
+        int leftCompatible = right;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            int desiredSum = nums[right] * (right - mid + 1);
+            int currentSum = (mid != 0) ? prefixSumArray[right] - prefixSumArray[mid - 1] : prefixSumArray[right];
+
+            if (desiredSum - currentSum <= k) {
+                leftCompatible = mid;
+                j = mid - 1;
+            } else
+                i = mid + 1;
+        }
+
+        return leftCompatible;
+    }
+
+    private static int[] prefixSumArray(int[] arr) {
+        // System.out.println(Arrays.toString(arr));
+
+        int[] brr = new int[arr.length];
+        brr[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            brr[i] = arr[i] + brr[i - 1];
+        }
+        // System.out.println(Arrays.toString(brr));
+        return brr;
     }
 }
