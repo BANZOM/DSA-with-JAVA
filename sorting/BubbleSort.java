04754fcd51e2a141d27454abc1db03ff86160f1a
Aditya Panwar <aditya.panwar.2208@gmail.com>
2024-12-07 21:39:10 +0530

    basic bubbleSort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -27,4 +27,17 @@ public class BubbleSort {
             if (!swapped) break;
         }
     }
+
+    static void bubbleSort2(int arr[]) {
+        int n = arr.length;
+        for(int i=0; i<n-1; i++) {
+            for(int j=i+1; j<n; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
 }
